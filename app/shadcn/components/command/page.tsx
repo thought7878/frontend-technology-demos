fe1dc01afa2c06c01bb39c5398b6c4a106015c91
Calm Andy <thought7878@qq.com>
2024-08-01 08:01:33 +0800

    feat: update command demo

--- app/shadcn/components/command/page.tsx
+++ app/shadcn/components/command/page.tsx
@@ -22,43 +22,62 @@ import {
 
 export default function CommandDemo() {
   return (
-    <Command className='rounded-lg border shadow-md'>
-      <CommandInput placeholder='Type a command or search...' />
-      <CommandList>
-        <CommandEmpty>No results found.</CommandEmpty>
-        <CommandGroup heading='Suggestions'>
-          <CommandItem onSelect={(value) => console.log('Selected', value)}>
-            <Calendar className='mr-2 h-4 w-4' />
-            <span>Calendar</span>
-          </CommandItem>
-          <CommandItem>
-            <Smile className='mr-2 h-4 w-4' />
-            <span>Search Emoji</span>
-          </CommandItem>
-          <CommandItem>
-            <Calculator className='mr-2 h-4 w-4' />
-            <span>Calculator</span>
-          </CommandItem>
-        </CommandGroup>
-        <CommandSeparator />
-        <CommandGroup heading='Settings'>
-          <CommandItem>
-            <User className='mr-2 h-4 w-4' />
-            <span>Profile</span>
-            <CommandShortcut>⌘P</CommandShortcut>
-          </CommandItem>
-          <CommandItem>
-            <CreditCard className='mr-2 h-4 w-4' />
-            <span>Billing</span>
-            <CommandShortcut>⌘B</CommandShortcut>
-          </CommandItem>
-          <CommandItem>
-            <Settings className='mr-2 h-4 w-4' />
-            <span>Settings</span>
-            <CommandShortcut>⌘S</CommandShortcut>
-          </CommandItem>
-        </CommandGroup>
-      </CommandList>
-    </Command>
+    <>
+      <Command className='rounded-lg border shadow-md'>
+        <CommandInput placeholder='Type a command or search...' />
+        <CommandList>
+          <CommandEmpty>No results found.</CommandEmpty>
+          <CommandGroup heading='Suggestions'>
+            <CommandItem onSelect={(value) => console.log('Selected', value)}>
+              <Calendar className='mr-2 h-4 w-4' />
+              <span>Calendar</span>
+            </CommandItem>
+            <CommandItem>
+              <Smile className='mr-2 h-4 w-4' />
+              <span>Search Emoji</span>
+            </CommandItem>
+            <CommandItem>
+              <Calculator className='mr-2 h-4 w-4' />
+              <span>Calculator</span>
+            </CommandItem>
+          </CommandGroup>
+          <CommandSeparator />
+          <CommandGroup heading='Settings'>
+            <CommandItem>
+              <User className='mr-2 h-4 w-4' />
+              <span>Profile</span>
+              <CommandShortcut>⌘P</CommandShortcut>
+            </CommandItem>
+            <CommandItem>
+              <CreditCard className='mr-2 h-4 w-4' />
+              <span>Billing</span>
+              <CommandShortcut>⌘B</CommandShortcut>
+            </CommandItem>
+            <CommandItem>
+              <Settings className='mr-2 h-4 w-4' />
+              <span>Settings</span>
+              <CommandShortcut>⌘S</CommandShortcut>
+            </CommandItem>
+          </CommandGroup>
+        </CommandList>
+      </Command>
+      <Command>
+        <CommandInput placeholder='Type a command or search...' />
+        <CommandList>
+          <CommandEmpty>No results found.</CommandEmpty>
+          <CommandGroup heading='Suggestions'>
+            <CommandItem>Calendar</CommandItem>
+            <CommandItem>Search Emoji</CommandItem>
+            <CommandItem>Calculator</CommandItem>
+          </CommandGroup>
+          <CommandSeparator />
+          <CommandGroup heading='Settings'>
+            <CommandItem>Profile</CommandItem>
+            <CommandItem>Billing</CommandItem>
+            <CommandItem>Settings</CommandItem>
+          </CommandGroup>
+        </CommandList>
+      </Command>
+    </>
   );
 }
