568cb522745d5c910eda551777d8ecb97cb0f98b
Calm Andy <thought7878@qq.com>
2024-08-20 08:34:01 +0800

    update

--- app/shadcn/components/form/page.tsx
+++ app/shadcn/components/form/page.tsx
@@ -1,6 +1,8 @@
 "use client";
 
+// 导入Button组件
 import { Button } from "@/components/ui/button";
+// 导入Form相关的组件和钩子
 import {
   Form,
   FormControl,
@@ -11,35 +13,45 @@ import {
   FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
+// 导入zodResolver用于表单验证
 import { zodResolver } from "@hookform/resolvers/zod";
+// 导入useForm钩子用于React表单处理
 import { useForm } from "react-hook-form";
+// 导入z用于定义表单的验证规则
 import { z } from "zod";
 
+// 定义表单的验证规则
 const formSchema = z.object({
+  // 用户名必须至少为2个字符
   username: z.string().min(2, {
     message: "Username must be at least 2 characters.",
   }),
 });
 
+// ProfileForm组件用于展示和处理用户表单
 export default function ProfileForm() {
-  // 1. Define your form.
+  // 1. 定义你的表单
   const form = useForm<z.infer<typeof formSchema>>({
+    // 使用zodResolver进行表单验证
     resolver: zodResolver(formSchema),
+    // 默认表单值
     defaultValues: {
       username: "",
     },
   });
 
-  // 2. Define a submit handler.
+  // 2. 定义一个提交处理函数
   function onSubmit(values: z.infer<typeof formSchema>) {
-    // Do something with the form values.
-    // ✅ This will be type-safe and validated.
+    // 对表单值进行处理
+    // ✅ 这将是类型安全和验证过的
     console.log(values);
   }
 
+  // 返回表单组件
   return (
     <Form {...form}>
       <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        {/* 用户名输入字段 */}
         <FormField
           control={form.control}
           name="username"
@@ -56,6 +68,7 @@ export default function ProfileForm() {
             </FormItem>
           )}
         />
+        {/* 提交按钮 */}
         <Button type="submit">Submit</Button>
       </form>
     </Form>
