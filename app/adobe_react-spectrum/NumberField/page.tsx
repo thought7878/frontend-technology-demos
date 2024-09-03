da9782c6d8f59df97cb7a2143de5d09c9d170d68
Calm Andy <thought7878@qq.com>
2024-09-03 08:45:41 +0800

    update

--- app/adobe_react-spectrum/NumberField/page.tsx
+++ app/adobe_react-spectrum/NumberField/page.tsx
@@ -1,11 +1,12 @@
 "use client";
-import { NumberField } from "@adobe/react-spectrum";
+import { NumberField, defaultTheme, Provider } from "@adobe/react-spectrum";
 
 export default function Page() {
   return (
+    // <Provider theme={defaultTheme}>
     <div>
       <NumberField
-        // TODO: TypeError: Cannot read properties of null (reading 'scale')
+        // TODO: If not use Provider, Bug---TypeError: Cannot read properties of null (reading 'scale')
         // TODO: BUG, 键盘修改值，不会触发 onChange ，直到失去焦点时。貌似不是Bug，https://react-spectrum.adobe.com/react-spectrum/NumberField.html#events
         // onChange={(value) => console.log(value)}
         label="Width"
@@ -13,5 +14,6 @@ export default function Page() {
         minValue={0}
       />
     </div>
+    // </Provider>
   );
 }
