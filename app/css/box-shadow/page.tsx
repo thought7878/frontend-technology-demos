ce25ae50294d17b46a1dfd87370590cd80c50d37
Calm Andy <thought7878@qq.com>
2024-09-11 21:04:37 +0800

    create

---
+++ app/css/box-shadow/page.tsx
@@ -0,0 +1,10 @@
+import "./page.css";
+
+export default function Page() {
+  return (
+    <div className="flex gap-10">
+      <button className="shadowed-button">点击我</button>
+      <button className="ring-2 ring-offset-2">点击我</button>
+    </div>
+  );
+}
