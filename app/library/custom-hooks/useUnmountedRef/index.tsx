7534fb5695c01fa6d7f1ddab51e486ec39dd2e23
Calm Andy <thought7878@qq.com>
2024-11-15 13:57:44 +0800

    add

---
+++ app/library/custom-hooks/useUnmountedRef/index.tsx
@@ -0,0 +1,18 @@
+// "use client";
+
+import { useEffect, useRef } from "react";
+
+const useUnmountedRef = (): { readonly current: boolean } => {
+  const unmountedRef = useRef<boolean>(false);
+
+  useEffect(() => {
+    unmountedRef.current = false;
+    return () => {
+      unmountedRef.current = true;
+    };
+  }, []);
+
+  return unmountedRef;
+};
+
+export default useUnmountedRef;
